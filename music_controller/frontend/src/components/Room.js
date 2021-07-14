cc72ef9d65717c14538d8e592f062ce57861b0ca
j q <BsMWgErTYR4LEDR9@protonmail.ch>
2021-07-14 17:05:09 -0400

    7

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -13,17 +13,17 @@ export default class Room extends Component {
         this.getRoomDetails;
     }
 
-    getRoomDetails() {
-        fetch('/api/get-room'+ '?code=' + this.roomCode).then((response) => 
-        response.json()
-        ).then((data) => {
-            this.setState({
-                votesToSkip: data.votes_to_skip,
-                guestCanPause: data.guest_can_pasue,
-                isHost: data.is_host,
+        getRoomDetails() {
+            fetch('/api/get-room'+ '?code=' + this.roomCode).then((response) => 
+            response.json()
+            ).then((data) => {
+                this.setState({
+                    votesToSkip: data.votes_to_skip,
+                    guestCanPause: data.guest_can_pasue,
+                    isHost: data.is_host,
+                });
             });
-        });
-    }
+        }
 
     render() {
         return (
@@ -31,7 +31,7 @@ export default class Room extends Component {
                 <h3>{this.roomCode}</h3>
                 <p>Votes: {this.state.votesToSkip}</p>
                 <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-                <p>Host: {this.state.isHost.toString}</p>
+                <p>Host: {this.state.isHost.toString()}</p>
             </div>
             );
         }
