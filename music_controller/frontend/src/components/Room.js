98c624e7457e7a3c557617380b097c142cf9ddb3
j q <BsMWgErTYR4LEDR9@protonmail.ch>
2021-07-16 21:18:42 -0400

    11 and 12

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,7 +1,6 @@
-import { Grid, Button, Typography } from "@material-ui/core";
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-
+import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class Room extends Component {
   constructor(props) {
@@ -13,11 +12,21 @@ export default class Room extends Component {
     };
     this.roomCode = this.props.match.params.roomCode;
     this.getRoomDetails();
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+    this.updateShowSettings = this.updateShowSettings.bind(this);
+    this.renderSettingsButton = this.renderSettingsButton.bind(this);
+    this.renderSettings = this.renderSettings.bind(this);
   }
 
   getRoomDetails() {
-    fetch("/api/get-room" + "?code=" + this.roomCode)
-      .then((response) => response.json())
+    return fetch("/api/get-room" + "?code=" + this.roomCode)
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push("/");
+        }
+        return response.json();
+      })
       .then((data) => {
         this.setState({
           votesToSkip: data.votes_to_skip,
@@ -27,22 +36,81 @@ export default class Room extends Component {
       });
   }
 
-  render() {
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push("/");
+    });
+  }
+
+  updateShowSettings(value) {
+    this.setState({
+      showSettings: value,
+    });
+  }
+
+  renderSettings() {
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
+          <CreateRoomPage 
+            update={true} 
+            votesToSkip={this.state.votesToSkip} 
+            guestCanPause={this.state.guestCanPause} 
+            roomCode={this.roomCode} 
+            updateCallBack={() => {}} 
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+        <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.updateShowSettings(false)}
+          >
+            Close
+          </Button>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={() => this.updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    )
+  }
+
+  render() {
+    if (this.state.showSettings) {
+      return this.renderSettings();
+    }
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
             Code: {this.roomCode}
           </Typography>
         </Grid>
         <Grid item xs={12} align="center">
           <Typography variant="h6" component="h6">
-          Votes: {this.state.votesToSkip}
+            Votes: {this.state.votesToSkip}
           </Typography>
         </Grid>
         <Grid item xs={12} align="center">
           <Typography variant="h6" component="h6">
-          Guest Can Pause: {this.state.guestCanPause.toString()}
+            Guest Can Pause: {this.state.guestCanPause.toString()}
           </Typography>
         </Grid>
         <Grid item xs={12} align="center">
@@ -50,12 +118,13 @@ export default class Room extends Component {
             Host: {this.state.isHost.toString()}
           </Typography>
         </Grid>
+        {this.state.isHost ? this.renderSettingsButton() : null}
         <Grid item xs={12} align="center">
-          <Button 
-          variant="contained" 
-          color="secondary" 
-          to="/"
-          component={Link}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
             Leave Room
           </Button>
         </Grid>
